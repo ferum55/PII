e741de3a0df4d723fd426fe647ba504fc6479cfc
ferum55 <ferko.roman59@gmail.com>
2024-04-23 17:34:19 +0300

    Commit for deletion

--- script.js
+++ script.js
@@ -42,12 +42,12 @@ function addNewStudentsTableRow(id, group, name, gender, birthday)
 {
   const newRow = document.createElement("tr");
   newRow.innerHTML = `
-            <td><input type="checkbox" class="table-checkbox"/></td>
+            <td><input type="checkbox" class="table-checkbox" aria-label="Select Student"/>
             <td>${group}</td>
             <td>${name}</td>
             <td>${gender}</td>
             <td>${(birthday)}</td> 
-            <td class="table-status"><span class="circle"></span></td>
+            <td class="table-status"><i class="fa-solid fa-circle"></i></td>
                 <td>
                   <button class="edit-button">
                     <img src="pencil.png" width="20" alt="cross" />
@@ -59,9 +59,9 @@ function addNewStudentsTableRow(id, group, name, gender, birthday)
 
   const tableBody = document.querySelector(".main-table tbody");
   tableBody.appendChild(newRow);
-
   newRow.querySelector(".edit-button").addEventListener("click", handleEdit);
   newRow.querySelector(".remove-button").addEventListener("click", handleRemove);
+  newRow.querySelector(".table-checkbox").addEventListener("click", handleRemove);
   newRow.setAttribute('data-id', id);
 }
 
@@ -179,47 +179,22 @@ addStudentForm.addEventListener('submit', (event) => {
     );
   }
 });
-/**
- * @param {string} requestUrl
- * @param {string} group - group field value
- * @param {string} firstName - firstName field value
- * @param {string} lastName - lastName field value
- * @param {string} gender - gender field value
- * @param {string} birthday - birthday field value
- * @param {function(string): void} actionAfterResponse - function,  which receives a response from the server as a parameter "responseText (string)" and performs actions on it
- * @returns {void}
-*/
 
+function addNewStudentServerRequest(requestUrl, group, firstName, lastName, gender, birthday, actionAfterResponse) {
+  const data = {
+      Group: group,
+      FirstName: firstName,
+      LastName: lastName,
+      Gender: gender,
+      Birthday: birthday
+  };
+  console.log("POST request data: \n\n", data);
 
-function addNewStudentServerRequest(requestUrl, group, firstName, lastName, gender, birthday, actionAfterResponse)
-{
-    const data = {
-        Group: group,
-        FirstName: firstName,
-        LastName: lastName,
-        Gender: gender,
-        Birthday: birthday
-    };
-    console.log("POST request data: \n\n", data);
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", requestUrl, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", requestUrl, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log('Success.\n\nResponse text:\n\n', xhr.responseText);
-                actionAfterResponse(xhr.responseText);
-            } else {
-                console.error('Error:', xhr.responseText);
-            }
-        }
-    };
-
-    xhr.send(JSON.stringify(data));
-    console.log("POST add student request sent to:", requestUrl);
-    xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) {
           if (xhr.status === 200) {
               console.log('Success.\n\nResponse text:\n\n', xhr.responseText);
@@ -231,7 +206,7 @@ function addNewStudentServerRequest(requestUrl, group, firstName, lastName, gend
   };
 
   xhr.send(JSON.stringify(data));
-  console.log("POST edit student request sent to:", requestUrl);
+  console.log("POST add student request sent to:", requestUrl);
 }
 
 function getAllStudentServerRequest(requestUrl, actionAfterResponse)
@@ -270,8 +245,8 @@ function editStudentServerRequest(requestUrl, id, group, firstName, lastName, ge
     const xhr = new XMLHttpRequest();
     xhr.open("POST", requestUrl, true);
     xhr.setRequestHeader("Content-Type", "application/json");
+}
 
-  }
 function handleEdit(event) {
     const row = event.target.closest('tr');
     const cells = row.querySelectorAll('td');
